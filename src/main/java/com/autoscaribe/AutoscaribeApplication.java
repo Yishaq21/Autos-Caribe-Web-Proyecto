6435cdb30ce915d93d738c3f970240003dc84a4c
IsaacGuevara <isaaceduardog@hotmail.com>
2026-07-05 22:48:06 -0600

    Primer commit proyecto Autos Caribe

---
+++ src/main/java/com/autoscaribe/AutoscaribeApplication.java
@@ -0,0 +1,13 @@
+package com.autoscaribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoscaribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoscaribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoscaribe/AutoscaribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoscaribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoscaribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
